2768eedf8c430e98cb1a83af2ca167168091e4fd
abdullha19 <154753763+abdullha19@users.noreply.github.com>
2026-01-29 13:03:48 +0200

    Add files via upload

---
+++ الكورس الخامس الدرس 16.cpp
@@ -0,0 +1,40 @@
+﻿// الكورس الخامس الدرس 16.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+void PrintAllFromAAAtoZZZ() {
+
+    string Word = "";
+    cout << "\n" << "**************************************" << "\n" << endl;
+    for (int i = 65; i <= 90 ; i++) {
+        for (int j = 65; j <=90; j++) {
+            for (int m = 65; m <=90; m++)
+            {
+				Word= Word +(1, char(i));
+				Word= Word +(1, char(j));
+				Word= Word +(1, char(m));
+				cout << Word << endl;
+				Word = "";
+            }
+        }
+        cout << endl;
+    }
+    cout << "\n" << "**************************************" << endl;
+
+
+
+
+}
+
+
+int main()
+{
+    
+	PrintAllFromAAAtoZZZ();
+
+}
+
