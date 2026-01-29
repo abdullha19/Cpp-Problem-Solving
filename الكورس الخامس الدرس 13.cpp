4612ca580b72298be38903054f3e5fde9f4c13af
abdullha19 <154753763+abdullha19@users.noreply.github.com>
2026-01-29 10:59:23 +0200

    Add files via upload

---
+++ الكورس الخامس الدرس 13.cpp
@@ -0,0 +1,38 @@
+﻿// الكورس الخامس الدرس 13.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int ReadPositiveNumber(string Message) {
+    int Number = 0;
+    do {
+        cout << Message << endl;
+        cin >> Number;
+    } while (Number < 0);
+    return Number;
+}
+void InvPrintInvertedNumberPattern(int Number) {
+
+
+    cout << "\n" << "**************************************" <<"\n" << endl;
+    for (int i = 1; i <= Number; i++) {
+        for (int j = 1; j <= i; j++) {
+            cout << i;
+        }
+        cout << endl;
+    }
+    cout << "\n" << "**************************************" << endl;
+
+
+
+
+}
+
+
+int main()
+{
+	InvPrintInvertedNumberPattern(ReadPositiveNumber("Please enter a positive number:"));
+}
+
