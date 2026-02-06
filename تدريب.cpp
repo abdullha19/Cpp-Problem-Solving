017ac29afecd1f8f77238b96af3e786964e56b8e
abdullha19 <154753763+abdullha19@users.noreply.github.com>
2026-02-06 22:47:33 +0200

    Add files via upload

---
+++ تدريب.cpp
@@ -0,0 +1,54 @@
+﻿// تدريب.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <string>
+#include <limits>
+using namespace std;
+
+short  EnterEncryptionKey( ) {
+    short EncryptionKey;
+    cout << "please enter the EncryptionKey" << endl;
+    cin >> EncryptionKey;
+    // discard the rest of the line so subsequent getline works correctly
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    return EncryptionKey;
+}
+
+string REadename() {
+    string Text;
+    cout << "Enter your name: ";
+    getline(cin, Text);
+    return Text;
+
+}
+string EncryptText(string Text, short EncryptionKey) {
+    for (size_t i = 0; i < Text.length(); ++i) {
+        Text[i] = char((int)Text[i] + EncryptionKey);
+    }
+    return Text;
+}
+
+string DncryptText(string Text, short EncryptionKey) {
+    for (size_t i = 0; i < Text.length(); ++i) {
+        Text[i] =  char((int)Text[i] - EncryptionKey);
+    }
+    return Text;
+}
+
+int main()
+{
+    short EncryptionKey = EnterEncryptionKey();
+    string TextAfterEncryption, TextAfterDecryption;
+    string Text = REadename();
+    TextAfterEncryption = EncryptText(Text, EncryptionKey);
+
+    TextAfterDecryption = DncryptText(TextAfterEncryption, EncryptionKey);
+
+    cout << "Original Text: " << Text << endl;
+
+    cout << "Encrypted Text: " << TextAfterEncryption << endl;
+
+    cout << "Decrypted Text: " << TextAfterDecryption << endl;
+}
+
