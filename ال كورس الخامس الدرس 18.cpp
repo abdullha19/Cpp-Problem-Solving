b442d2b0b19644a17d74d328246b476e0b2d62e9
abdullha19 <154753763+abdullha19@users.noreply.github.com>
2026-02-06 22:16:58 +0200

    Add files via upload

---
+++ ال كورس الخامس الدرس 18.cpp
@@ -0,0 +1,65 @@
+﻿// ال كورس الخامس الدرس 18.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <string>
+using namespace std;
+string ReadText()
+{
+    string Text;  
+
+    cout << "Please enter Text?\n"; 
+    getline(cin, Text);              
+
+    return Text; 
+}
+
+
+string EncryptText(string Text, short EncryptionKey)
+{
+    
+    for (int i = 0; i <= Text.length(); i++)
+    {
+       
+        Text[i] = char((int)Text[i] + EncryptionKey);
+    }
+    return Text; 
+}
+
+
+string DecryptText(string Text, short EncryptionKey)
+{
+    
+    for (int i = 0; i <= Text.length(); i++)
+    {
+        
+        Text[i] = char((int)Text[i] - EncryptionKey);
+    }
+    return Text; 
+}
+
+int main()
+{
+    const short EncryptionKey = 2; 
+
+    string TextAfterEncryption, TextAfterDecryption;  
+
+    
+    string Text = ReadText();
+
+    
+    TextAfterEncryption = EncryptText(Text, EncryptionKey);
+
+    
+    TextAfterDecryption = DecryptText(TextAfterEncryption, EncryptionKey);
+
+    
+    cout << "\nText Before Encryption : " << Text << endl;
+    
+    cout << "Text After Encryption  : " << TextAfterEncryption << endl;
+    
+    cout << "Text After Decryption  : " << TextAfterDecryption << endl;
+
+    return 0; 
+}
+
