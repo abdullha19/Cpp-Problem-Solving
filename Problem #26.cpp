7aa954bce6549dc80adbee21f534d2d0ea3a1d40
abdullha19 <154753763+abdullha19@users.noreply.github.com>
2026-02-15 02:05:37 +0200

    Add files via upload

---
+++ Problem #26.cpp
@@ -0,0 +1,93 @@
+// Problem #26.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+
+#include <iostream>
+#include <cstdlib> 
+#include <ctime>   
+
+using namespace std;
+
+int ReadPositiveNumber(string Message) {
+    int Number = 0;
+    do
+    {
+        cout << Message << endl;
+        cin >> Number;
+    } while (Number <= 0);
+
+    return Number;
+}
+
+void FillArrayWithRandomNumbers(int arr[100], int& arrLength) {
+    if (arrLength > 100) {
+        arrLength = 100;
+        cout << "Note: Array size limited to 100." << endl;
+    }
+
+    for (int i = 0; i < arrLength; i++)
+    {
+        arr[i] = rand() % 100 + 1;
+    }
+}
+
+void PrintArray(int arr[100], int arrLength) {
+    for (int i = 0; i < arrLength; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int GetMaxNumber(int arr[100], int arrLength) {
+    int max = arr[0];
+    for (int i = 1; i < arrLength; i++) {
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+    }
+    return max;
+}
+
+int GetMinNumber(int arr[100], int arrLength) {
+    int min = arr[0];
+    for (int i = 1; i < arrLength; i++) {
+        if (arr[i] < min) {
+            min = arr[i];
+        }
+    }
+    return min;
+}
+
+int sumArray(int arr[100], int arrLength) {
+    int sum = 0;
+    for (int i = 0; i < arrLength; i++) {
+        sum += arr[i];
+    }
+    return sum;
+}
+
+float GetAverage(int arr[100], int arrLength) {
+    return (float)sumArray(arr, arrLength) / arrLength;
+}
+
+int main() {
+    srand((unsigned)time(NULL));
+
+    int arr[100];
+    int arrLength;
+
+    arrLength = ReadPositiveNumber("Please enter the number of elements in the array:");
+
+    FillArrayWithRandomNumbers(arr, arrLength);
+
+    cout << "\nArray Elements: ";
+    PrintArray(arr, arrLength);
+
+    cout << "\nMaximum Number in the Array: " << GetMaxNumber(arr, arrLength) << endl;
+    cout << "Minimum Number in the Array: " << GetMinNumber(arr, arrLength) << endl;
+    cout << "Sum of Array Elements: " << sumArray(arr, arrLength) << endl;
+    cout << "Average of Array Elements: " << GetAverage(arr, arrLength) << endl;
+
+    return 0;
+}
