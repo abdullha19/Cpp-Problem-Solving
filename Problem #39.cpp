ee08f08947e96b0800765454560c880a7c414247
abdullha19 <154753763+abdullha19@users.noreply.github.com>
2026-02-17 02:16:51 +0200

    Add files via upload

--- Problem #39.cpp
+++ Problem #39.cpp
@@ -1,20 +1,28 @@
-// Problem #39.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
-
 #include <iostream>
 #include <ctime>
 #include <string>
-using namespace std;
+using namespace std;  
 
+enum enPrimNotPrime { Prime = 1, NotPrime = 2 };
 
+enPrimNotPrime CheckPrime(int Number)
+{
+    int M = round(Number / 2);
+
+    for (int Counter = 2; Counter <= M; Counter++)
+    {
+        if (Number % Counter == 0)
+            return enPrimNotPrime::NotPrime;
+    }
+
+    return enPrimNotPrime::Prime;
+}
 
 int RandomNumber(int From, int To)
 {
     int randNum = rand() % (To - From + 1) + From;
     return randNum;
 }
-
 
 void FillArrayWithRandomNumbers(int arr[100], int& arrLength)
 {
@@ -24,7 +32,6 @@ void FillArrayWithRandomNumbers(int arr[100], int& arrLength)
     for (int i = 0; i < arrLength; i++)
         arr[i] = RandomNumber(1, 100);
 }
-
 
 void PrintArray(int arr[100], int arrLength)
 {
@@ -39,53 +46,13 @@ void AddArrayElement(int Number, int arr[100], int& arrLength)
     arr[arrLength - 1] = Number;
 }
 
-
-void CopyOddNumbers(int arrSource[100], int arrDestination[100], int arrLength, int& arrDestinationLength)
+void CopyPrimeNumbers(int arrSource[100], int arrDestination[100], int arrLength, int& arrDestinationLength)
 {
     for (int i = 0; i < arrLength; i++)
     {
-        if (arrSource[i] % 2 != 0)
+        if (CheckPrime(arrSource[i]) == enPrimNotPrime::Prime)
         {
             AddArrayElement(arrSource[i], arrDestination, arrDestinationLength);
-        }
-    }
-}
-void primarrenumber (int arr[100], int arrLength) {
-    for (int i = 0; i < arrLength; i++) {
-        bool isPrime = true;
-        if (arr[i] <= 1) {
-            isPrime = false;
-        }
-        else {
-            for (int j = 2; j <= arr[i] / 2; j++) {
-                if (arr[i] % j == 0) {
-                    isPrime = false;
-                    break;
-                }
-            }
-        }
-        if (isPrime) {
-            cout << arr[i] << " is a prime number." << endl;
-        }
-    }
-}
-
-void primarrenumberwithAddArrayElement(int arr[100], int arrLength, int primeArr[100], int& primeArrLength) {
-    for (int i = 0; i < arrLength; i++) {
-        bool isPrime = true;
-        if (arr[i] <= 1) {
-            isPrime = false;
-        }
-        else {
-            for (int j = 2; j <= arr[i] / 2; j++) {
-                if (arr[i] % j == 0) {
-                    isPrime = false;
-                    break;
-                }
-            }
-        }
-        if (isPrime) {
-            AddArrayElement(arr[i], primeArr, primeArrLength);
         }
     }
 }
@@ -95,25 +62,18 @@ int main() {
 
     int arr[100];
     int arrLength = 0;
-
-    FillArrayWithRandomNumbers(arr, arrLength);
-
     int arr2[100];
     int arr2Length = 0;
 
-    int primeArr[100];
-	int primeArrLength = 0;
+    FillArrayWithRandomNumbers(arr, arrLength);
 
-    CopyOddNumbers(arr, arr2, arrLength, arr2Length);
+    CopyPrimeNumbers(arr, arr2, arrLength, arr2Length);
+
     cout << "\nArray 1 elements:\n";
     PrintArray(arr, arrLength);
-	primarrenumberwithAddArrayElement(arr, arrLength, primeArr, primeArrLength);
-    cout << "\nArray 2 Odd numbers:\n";
+
+    cout << "\nArray 2 Prime numbers:\n";
     PrintArray(arr2, arr2Length);
-    
-	cout << "\nPrime numbers in Array 1:\n";
-	primarrenumber(arr, arrLength);
-	cout << "\nPrime numbers in Array 1 added to a new array:\n";
-	PrintArray(primeArr, primeArrLength);
+
     return 0;
 }
